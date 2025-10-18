9f3ac96a8614eccee96286ec292c6449c8b8e35a
Joanna2954 <joannapraisy29@gmail.com>
2025-10-18 18:02:14 +0530

    Uncomment serializer classes in serializer.py
    Uncommented serializer classes for Car and Rental models.

--- Car_Rental_Backend/my_project/my_app/serializer.py
+++ Car_Rental_Backend/my_project/my_app/serializer.py
@@ -1,23 +1,3 @@
-# from rest_framework import serializers
-# from .models import Car, Rental
-
-
-# class CarSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Car
-#         fields = "__all__"     
-#         depth = 1
-
-
-# class RentalSerializer(serializers.ModelSerializer):
-   
-
-#     class Meta:
-#         model = Rental
-#         fields = "__all__"    
-#         extra_kwargs = {'car': {'read_only': True}} 
-#         depth = 1
-
 
 
 
